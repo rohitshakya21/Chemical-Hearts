bfdf789c2df8c778810a02873eb27c49d1d0407c
rohitshakya21 <51616336+rohitshakya21@users.noreply.github.com>
2021-10-21 12:10:36 +0530

    gif added

--- box.js
+++ box.js
@@ -18,17 +18,18 @@ const buttonTwo = document.querySelector('#button-two');
 buttonTwo.addEventListener('click', (e) => {
 	document.querySelector('#button-two').style.background = '#9fe6a0';
 	document.getElementById('text').textContent = 'On Amazon Prime';
-	document.h1.style.backgroundImage = "url('https://wallpapercave.com/wp/wp7576881.jpg')";
+	document.getElementById('body').style.background = '#000';
 })
 
 
 const buttonThree = document.getElementById('button-three');
 buttonThree.addEventListener('click', (e) => {
-	document.getElementById('button-three').textContent = 'BTS fan?';
+	//document.getElementById('button-three').textContent = 'BTS fan?';
 	document.getElementById('button-three').style.fontStyle = 'Italic';
 
 	document.getElementById('button-three').style.background = '#faf3f3';
 	document.getElementById('body').style.background = '#867ae9';
+	document.body.style.backgroundImage = "url('chemgif1.gif')";
 })
 
 const buttonFour = document.getElementById('button-four');
